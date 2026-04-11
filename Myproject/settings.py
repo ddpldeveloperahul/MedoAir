cad421d6f78e98e57cfc128367be07022226d824
dyfolab <dyfolabs@gmail.com>
2026-04-11 17:42:35 +0530

    add the new database

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -93,13 +93,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medoair_db',
+        'USER': 'postgres',
+        'PASSWORD': 'rahul',
+        'HOST': 'localhost',  # or your DB server IP
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
